d02840211bfc443d08cb3b2492c03676568b58de
pujabhat <pooja.bhat33333@gmail.com>
2019-05-06 19:27:51 +0545

    c++ codes

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main(){
+	char name[10];
+	cout<<"Enter your name:";
+	cin>>name;
+    cout<<"This is my name :"<<name;
+    return 0;
+}
